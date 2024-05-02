9f9139b0a426d42886383aff768e5899f5773b94
YakGalaxy <alexlaycy@gmail.com>
2024-05-02 20:27:52 +0100

    Added delete button which updates list

--- react-mini-project/src/components/TodoList.jsx
+++ react-mini-project/src/components/TodoList.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
-import Tasks from '../assets/tasks.json'
+import React, { useState } from "react";
+import Tasks from "../assets/tasks.json";
 
 function TodoList() {
-    const [task, setTask] = useState(Tasks)
-    // const [isCompleted, setIsCompleted] = useState(false); 
+  const [task, setTask] = useState(Tasks);
   return (
-      <>
-          
+    <>
           {task.map((oneTask) => {
-              return (oneTask.completed && <li>{oneTask.task}</li>)
-              }
-          )}
+        return (
+          oneTask.completed && (
+            <>
+              <li>{oneTask.task}</li>
+             <button onClick={() => setTask(oneTask.completed = true)} className="delete-button">Delete Task</button>
+            </>
+          )
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
